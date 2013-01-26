69ee869c530b99813638ff3ab8e6d6bdef2da339
XXFatai <trf669045207@126.com>
2013-01-26 16:11:58 +0800

    到底什么情况啊！

---
+++ src/com/trf/prime.java
@@ -0,0 +1,44 @@
+
+/**********************************************************
+ *  Copyright (C),2013, Hebei North University.
+ * PrjName：   prime
+ * FileName: prime.java
+ * Author:   润发
+ * Date:     2013-1-26 下午3:21:18  
+ * Version :
+ * Description:
+ **********************************************************/
+package com.trf;
+import java.util.Scanner;
+
+public class prime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		Decomposition dec = new Decomposition();
+		
+		System.out.print("输入一个正整数：");
+		dec.decompose(input.nextInt());
+		input.close();
+	}
+
+}
+class Decomposition{
+	public void decompose(int n){
+		int i = 2;
+
+		System.out.print(n+"=");
+		for(; i <= n;){
+			if(i == n){
+				System.out.print(i);
+			}
+			else if (n % i == 0){
+				System.out.print(i+"*");
+				n = n / i;
+			}
+			else 
+				i++;
+		}
+	}
+}
